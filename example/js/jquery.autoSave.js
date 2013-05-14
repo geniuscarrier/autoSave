918a7068a6621e3387b934ef3cf098afbf0d66d0
Yang Zhao <geniuscarrier@gmail.com>
2013-05-13 23:10:26 -0700

    Fix demo page for Firefox

--- example/js/jquery.autoSave.js
+++ example/js/jquery.autoSave.js
@@ -8,7 +8,7 @@
         return this.each(function() {
             var timer = 0, 
                 $this = $(this),
-                delay = ms || 2000;
+                delay = ms || 1000;
             $this.keyup(function() {
                 clearTimeout(timer);
                 var $context = $this.val();
